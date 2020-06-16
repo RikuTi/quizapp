647939b86ccd7fff24159923d63aeaa882467ad8
Riku Tiitta <riku.tiitta@edu.karelia.fi>
2020-06-16 13:25:08 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header 
+    :numCorrect="numCorrect"
+    :numTotal="numTotal"
+    />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+
+          <QuestionBox
+            v-if="questions.length && index < 1"
+            :currentQuestion="questions[index]"
+            :next ="next"
+            :increment="increment"
+            :index="index"
+           />
+
+           <VideoContainer
+           v-if="index >= 1"
+           />
+          
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
+import VideoContainer from './components/VideoContainer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox,
+    VideoContainer
+  },
+
+  data() {
+      return {
+          questions : [],
+          myQuestions: [],
+          index: 0,
+          numCorrect: 0,
+          numTotal: 0
+      }
+  },
+  methods: {
+      next() {
+          this.index++;
+      },
+      increment(isCorrect) {
+          if(isCorrect) {
+              this.numCorrect++
+          }
+          //this.numTotal++
+      }
+  },
+  mounted: function() {
+      /*fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple', {
+          method: 'get'
+      })
+      .then((response) => {
+          return response.json()
+      })
+      .then((jsonData) => {
+          this.questions = jsonData.results
+      })*/
+      var question_1 = {correct_answer:"test", incorrect_answers: ["not test","real test","a test"],question:"what is the test type of test?", image_source: "https://i.pinimg.com/236x/cb/c8/7c/cbc87ce85795375cfbf2604e8fa725c2--cavalli-blue-cats.jpg" }
+
+      this.questions.push(question_1)
+      this.numTotal = this.questions.length
+      
   }
 }
 </script>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+
+      <template v-slot:lead>
+        {{  decoder(currentQuestion.question) }}
+
+        <div v-if="currentQuestion.image_source != ''">
+          <img :src="currentQuestion.image_source" alt="">
+        </div>
+      </template>
+
+      <hr class="my-4">
+
+
+      <b-list-group>
+     
+          <b-list-group-item  v-for="(answer, index) in answers" :key="index" 
+          @click.prevent="selectAnswer(index)"
+          :disabled="answered"
+          :class="answerClass(index)"
+          >
+
+          {{ answer }}
+          </b-list-group-item>
+       
+      </b-list-group>
+
+      <b-button 
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+        >
+        Submit
+
+      </b-button>
+      <b-button @click="next" :disabled="!answered" variant="success">
+        Next
+      </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+export default {
+  props : {
+    currentQuestion: Object,
+    next: Function,
+    increment: Function
+  },
+  data () {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
+  },
+  watch: {
+    currentQuestion: {
+     immediate: true,
+     handler() {
+        this.selectedIndex = null
+        this.shuffleAnswers()
+        this.answered = false
+     }
+    }
+  },
+  methods: {
+      selectAnswer(index) {
+        this.selectedIndex = index
+      },
+      shuffleAnswers() {
+        let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+        this.shuffledAnswers = _.shuffle(answers)
+        this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+      },
+      submitAnswer() {
+        let isCorrect = false
+        if(this.selectedIndex === this.correctIndex) {
+          isCorrect = true
+        }
+
+        this.increment(isCorrect)
+
+        this.answered = true
+      },
+      answerClass(index) {
+        let answerClass = ''
+        
+        if(!this.answered && this.selectedIndex === index) {
+          answerClass = 'selected'
+        } else if(this.answered && this.correctIndex === index) {
+          answerClass = 'correct'
+        } else if(this.answered && this.selectedIndex === index && this.correctIndex !== index) {
+          answerClass = 'incorrect'
+        }
+
+        return answerClass
+      },
+     decoder(str) {
+       let textArea = document.createElement('textarea')
+       textArea.innerHTML = str
+       return textArea.value
+     }
+  }
+}
+</script>
+
+
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+
+  .list-group-item:hover {
+    background: #eeeeee;
+    cursor: pointer;
+  }
+
+  .btn {
+    margin: 0 5px;
+  }
+
+  .selected {
+    background-color: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect {
+    background-color: red;
+  }
+
+</style>
